d28c7f07ed2cc3ad509e8af4bc7b0bdbaf9dad56
Juan Lemos <jlemos329@gmail.com>
2025-10-27 18:34:20 -0300

    ok

--- estudando/estudando pandas/drive-download-20251024T210938Z-1-001/main.py
+++ estudando/estudando pandas/drive-download-20251024T210938Z-1-001/main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 #dataframe = pd.DataFrame()
 
-venda = {'data': ['15/02/2021', '16/02/2021'],
+"""venda = {'data': ['15/02/2021', '16/02/2021'],
          'valor': [500, 300],
          'produto': ['feijao', 'arroz'],
          'qtde': [50, 70],
         }
 vendas_df = pd.DataFrame(venda)
-print(vendas_df)
+
+print(vendas_df)"""
+
